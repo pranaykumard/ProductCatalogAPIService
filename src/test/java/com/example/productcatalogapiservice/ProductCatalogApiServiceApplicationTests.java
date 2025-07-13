a1a19587063bec9394bc70d8a47085e3d5c08cb9
pranay <pranaykumar820@gmail.com>
2025-07-13 20:12:09 +0530

    Added spring boot and dependencies

---
+++ src/test/java/com/example/productcatalogapiservice/ProductCatalogApiServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.productcatalogapiservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductCatalogApiServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
